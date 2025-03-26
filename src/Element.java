272338501c379d8ce8d8aa620af694b73307330e
linmjie <linmgjie@gmail.com>
2025-03-25 21:34:36 -0400

    prototype

--- src/Element.java
+++ src/Element.java
@@ -2,10 +2,21 @@ public class Element {
     private String elementName;
     private float atomicMass;
     private int atomicNumber;
+
+    private String ionName;
+    private int charge;
     public Element (String elementName, float atomicMass, int atomicNumber){
         this.elementName=elementName;
         this.atomicMass=atomicMass;
         this.atomicNumber=atomicNumber;
+    }
+    //for ions
+    public Element(String ionName, int charge, String elementName, float atomicMass, int atomicNumber){
+        this.elementName=elementName;
+        this.atomicMass=atomicMass;
+        this.atomicNumber=atomicNumber;
+        this.ionName=ionName;
+        this.charge=charge;
     }
     //no atomic mass
     public Element (String elementName, int atomicNumber){
@@ -22,8 +33,21 @@ public class Element {
     public int getAtomicNumber (){
         return atomicNumber;
     }
+
+    public String getIonName(){
+        return ionName;
+    }
+    public int getCharge(){
+        return charge;
+    }
     @Override
     public String toString(){
-        return "Element Name: " + getElementName() + " , " + "Atomic Number: " + getAtomicNumber() + " , " + "Atomic Mass: " + getAtomicMass();
+        if (this.ionName==null)
+            //not ion
+            return "Element Name: " + getElementName() + " , " + "Atomic Number: " + getAtomicNumber() + " , " + "Atomic Mass: " + getAtomicMass();
+        else
+            //ion
+            return "Ion Name: " +  getIonName() + " , " + "Element Name: " + "Ion Charge: " + getCharge() + "," +
+                    getElementName() + " , " + "Atomic Number: " + getAtomicNumber() + " , " + "Atomic Mass: " + getAtomicMass();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,49 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main{
+    static HashMap<String, Element> element = new HashMap<>(Symbol.createPeriodicTable());
     public static void main(String[] args) {
-        HashMap<String, Element> element = new HashMap<>(Symbol.createPeriodicTable());
-        System.out.println(element.get("F"));
+        //take input
+        System.out.println("Type \"quit\" to end program");
+        String input = "";
+        while (!input.equalsIgnoreCase("quit")){
+            {System.out.print("Input: ");
+                Scanner mainInput = new Scanner(System.in);
+                input = mainInput.next();
+                if (input.equalsIgnoreCase("quit"))
+                    break;
+                if (!checkInputValidity(input)) {
+                    System.out.println("please input something valid");
+                    continue;
+                }
+                input=input.trim();
+                System.out.println("Your input: " + input);}
+            //process
+            System.out.println("Molar Mass of " + input + ": " + findMolarMass(input));
+        }
+    }
+    public static float findMolarMass(String formula){
+        float molarMass=0F;
+        for(int i=0; i<formula.length(); i++){
+            String symbol = formula.substring(i, i + 1);
+            int subscript=1;
+            i++;
+            if ((formula.length()>i)&&Character.isLowerCase(formula.charAt(i))) {
+                symbol+=formula.charAt(i);
+            } else
+                i--;
+            i++;
+            if ((formula.length()>i)&&Character.isDigit(formula.charAt(i))) {
+                subscript = Character.getNumericValue(formula.charAt(i));
+            } else
+                i--;
+            molarMass+=element.get(symbol).getAtomicMass()*subscript;
+        }
+        return molarMass;
+    }
+    public static boolean checkInputValidity(String input){
+        //placeholder
+        return true;
     }
 }
